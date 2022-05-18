f4d2720859254283b6aeb8a621cf1a270af4c3b4
DESKTOP-D967RNS\Admin <dmariks@gmail.com>
2022-05-18 17:37:34 +0300

    add self.wait_until_displayed for 5 tests

--- pages/start_page.py
+++ pages/start_page.py
@@ -49,35 +49,35 @@ class StartPage(BasePage):
         self.fill_field(xpath=self.constants.SIGN_UP_USERNAME_XPATH, value=user.username2sym)
         self.fill_field(xpath=self.constants.SIGN_UP_EMAIL_XPATH, value=user.email)
         self.fill_field(xpath=self.constants.SIGN_UP_PASSWORD_XPATH, value=user.password)
-        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_3_CHARACTERS_TEXT)
+        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_REGISTER_FORM_XPATH)
         self.click(xpath=self.constants.SIGN_UP_BUTTON_XPATH)
 
     def input_empty_field_username(self, user):
         self.fill_field(xpath=self.constants.SIGN_UP_USERNAME_XPATH, value=user.usernameempty)
         self.fill_field(xpath=self.constants.SIGN_UP_EMAIL_XPATH, value=user.email)
         self.fill_field(xpath=self.constants.SIGN_UP_PASSWORD_XPATH, value=user.password)
-        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_3_CHARACTERS_TEXT)
         self.click(xpath=self.constants.SIGN_UP_BUTTON_XPATH)
+        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_REGISTER_FORM_XPATH)
 
     def input_31symbols_field_username(self, user):
         self.fill_field(xpath=self.constants.SIGN_UP_USERNAME_XPATH, value=user.username31sym)
         self.fill_field(xpath=self.constants.SIGN_UP_EMAIL_XPATH, value=user.email)
         self.fill_field(xpath=self.constants.SIGN_UP_PASSWORD_XPATH, value=user.password)
-        sleep(1)
+        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_REGISTER_FORM_XPATH)
         self.click(xpath=self.constants.SIGN_UP_BUTTON_XPATH)
 
     def input_space_field_username(self, user):
         self.fill_field(xpath=self.constants.SIGN_UP_USERNAME_XPATH, value=user.usernamespace)
         self.fill_field(xpath=self.constants.SIGN_UP_EMAIL_XPATH, value=user.email)
         self.fill_field(xpath=self.constants.SIGN_UP_PASSWORD_XPATH, value=user.password)
-        sleep(1)
+        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_REGISTER_FORM_XPATH)
         self.click(xpath=self.constants.SIGN_UP_BUTTON_XPATH)
 
     def input_special_symbol_field_username(self, user):
         self.fill_field(xpath=self.constants.SIGN_UP_USERNAME_XPATH, value=user.usernamespecial)
         self.fill_field(xpath=self.constants.SIGN_UP_EMAIL_XPATH, value=user.email)
         self.fill_field(xpath=self.constants.SIGN_UP_PASSWORD_XPATH, value=user.password)
-        sleep(1)
+        self.wait_until_displayed(xpath=self.constants.ERROR_MESSAGE_REGISTER_FORM_XPATH)
         self.click(xpath=self.constants.SIGN_UP_BUTTON_XPATH)
 
     def verify_error_message_3symbols_field_username(self):
@@ -100,8 +100,8 @@ class StartPage(BasePage):
         assert self.driver.find_element(by=By.XPATH,
                                         value=self.constants.SUCCESS_MESSAGE_USERNAME_XPATH).text == username.lower()
 
-        # FixMe: Move to ProfilePage object once created
-        @log_wrapper
-        def logout(self):
-            """Sign out from user profile"""
-            self.click(self.constants.SIGN_OUT_BUTTON_XPATH)
+    # FixMe: Move to ProfilePage object once created
+    @log_wrapper
+    def logout(self):
+        """Sign out from user profile"""
+        self.click(self.constants.SIGN_OUT_BUTTON_XPATH)
